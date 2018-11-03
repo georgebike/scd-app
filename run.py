34d2efef9359306449f21e8649f5414afb58230c
George Catargiu <georgecatargiu96@gmail.com>
2018-11-03 23:46:06 +0200

    new update for github

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     env_name = os.getenv('FLASK_ENV')   # get the environment (development/production) from env variables)
     print(env_name)
     app = create_app(env_name)          # create the app with the corresponding environment
-    app.run(host='0.0.0.0', port=80)                           # run the app
+    port = os.getenv('PORT')
+    app.run(host='0.0.0.0', port=port)                           # run the app
